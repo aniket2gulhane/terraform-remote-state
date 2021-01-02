d5a1ce28c3a04eefb7e2d130791c2bd10b32c101
Aniket <aniket.gulhane@cellpointmobile.com>
2021-01-02 11:35:53 +0530

    finale changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "aniket-terraform-backend-state"  
+  bucket = "aniket-terraform-backend-state100"  
   versioning {
     enabled = true
   }  
